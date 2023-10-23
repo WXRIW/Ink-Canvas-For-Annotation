aad7ca5aaa59814673ae94b52ebb615fff9b2ff3
ChangSakura <ChangSakura@outlook.com>
2023-10-24 00:39:31 +0800

    add project files

---
+++ Ink Canvas/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using AutoUpdaterDotNET;
+using Ink_Canvas.Helpers;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+
+namespace Ink_Canvas
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        System.Threading.Mutex mutex;
+
+        public static string[] StartArgs = null;
+        public static string RootPath = Environment.GetEnvironmentVariable("APPDATA") + "\\Ink Canvas\\";
+
+        public App()
+        {
+            this.Startup += new StartupEventHandler(App_Startup);
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            Ink_Canvas.MainWindow.ShowNewMessage("发生未预期的异常，可能导致软件运行不稳定。\n建议保存墨迹后重启软件。", true);
+            LogHelper.NewLog(e.Exception.ToString());
+            e.Handled = true;
+        }
+
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            RootPath = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+
+            LogHelper.NewLog(string.Format("Ink Canvas Starting (Version: {0})", Assembly.GetExecutingAssembly().GetName().Version.ToString()));
+
+            bool ret;
+            mutex = new System.Threading.Mutex(true, "Ink_Canvas", out ret);
+
+            if (!ret && !e.Args.Contains("-m")) //-m multiple
+            {
+                LogHelper.NewLog("Detected existing instance");
+                MessageBox.Show("已有一个程序实例正在运行");
+                LogHelper.NewLog("Ink Canvas automatically closed");
+                Environment.Exit(0);
+            }
+
+            StartArgs = e.Args;
+        }
+    }
+}
---
+++ Ink Canvas/Helpers/TimeMachine.cs
@@ -0,0 +1,218 @@
+﻿using System.Collections.Generic;
+using System.Windows.Ink;
+
+namespace Ink_Canvas.Helpers
+{
+    public class TimeMachine
+    {
+        private readonly List<TimeMachineHistory> _allHistory = new List<TimeMachineHistory>();
+        private int _currentIndex = -1;
+
+        private int _currentImageId = -1;
+
+        public delegate void OnUndoStateChange(bool status);
+
+        public event OnUndoStateChange OnUndoStateChanged;
+
+        public delegate void OnRedoStateChange(bool status);
+
+        public event OnRedoStateChange OnRedoStateChanged;
+
+        public void CommitStrokeUserInputHistory(StrokeCollection stroke)
+        {
+            _allHistory.Add(new TimeMachineHistory(stroke, TimeMachineHistoryType.UserInput, false));
+            _currentIndex = _allHistory.Count - 1;
+            NotifyUndoRedoState();
+        }
+
+        public void CommitStrokeShapeHistory(StrokeCollection strokeToBeReplaced, StrokeCollection generatedStroke)
+        {
+            _allHistory.Add(new TimeMachineHistory(generatedStroke, TimeMachineHistoryType.ShapeRecognition, false, strokeToBeReplaced));
+            _currentIndex = _allHistory.Count - 1;
+            NotifyUndoRedoState();
+        }
+
+        public void CommitStrokeRotateHistory(StrokeCollection strokeToBeReplaced, StrokeCollection generatedStroke)
+        {
+            _allHistory.Add(new TimeMachineHistory(generatedStroke, TimeMachineHistoryType.Rotate, false, strokeToBeReplaced));
+            _currentIndex = _allHistory.Count - 1;
+            NotifyUndoRedoState();
+        }
+
+        public void CommitStrokeEraseHistory(StrokeCollection stroke, StrokeCollection sourceStroke = null)
+        {
+            _allHistory.Add(new TimeMachineHistory(stroke, TimeMachineHistoryType.Clear, true, sourceStroke));
+            _currentIndex = _allHistory.Count - 1;
+            NotifyUndoRedoState();
+        }
+
+        public void CommitStrokeImageHistory(byte[] imageBytes, double x, double y, double width, double height)
+        {
+            _currentImageId++;
+            _allHistory.Add(new TimeMachineHistory(imageBytes, x, y, width, height, TimeMachineHistoryType.ImageInsert, _currentImageId));
+            _currentIndex = _allHistory.Count - 1;
+            NotifyUndoRedoState();
+        }
+        public void CommitImageDeleteHistory(int imageId)
+        {
+            _allHistory.Add(new TimeMachineHistory(TimeMachineHistoryType.ImageRomove ,imageId));
+            _currentIndex = _allHistory.Count - 1;
+            NotifyUndoRedoState();
+        }
+        public void CommitImageChangeHistory(double x, double y, double width, double height, int imageId)
+        {
+            _allHistory.Add(new TimeMachineHistory(x, y, width, height, imageId));
+            _currentIndex = _allHistory.Count - 1;
+            NotifyUndoRedoState();
+        }
+
+        public void ClearStrokeHistory()
+        {
+            _allHistory.Clear();
+            _currentIndex = -1;
+            NotifyUndoRedoState();
+        }
+
+        public TimeMachineHistory Undo()
+        {
+            if (_currentIndex >= 0)
+            {
+                var item = _allHistory[_currentIndex];
+                item.StrokeHasBeenCleared = !item.StrokeHasBeenCleared;
+                _currentIndex--;
+                OnUndoStateChanged?.Invoke(_currentIndex > -1);
+                OnRedoStateChanged?.Invoke(_allHistory.Count - _currentIndex - 1 > 0);
+                return item;
+            }
+            return null;
+        }
+
+        public TimeMachineHistory Redo()
+        {
+            if (_currentIndex < _allHistory.Count - 1)
+            {
+                var item = _allHistory[++_currentIndex];
+                item.StrokeHasBeenCleared = !item.StrokeHasBeenCleared;
+                NotifyUndoRedoState();
+                return item;
+            }
+            return null;
+        }
+
+        public TimeMachineHistory[] ExportTimeMachineHistory()
+        {
+            return _allHistory.ToArray();
+        }
+
+        public bool ImportTimeMachineHistory(TimeMachineHistory[] sourceHistory)
+        {
+            // 清除所有历史记录
+            _allHistory.Clear();
+            _currentIndex = -1;
+            
+            // 遍历导入的历史记录
+            foreach (var history in sourceHistory)
+            {
+                _allHistory.Add(history);
+            }
+            _currentIndex = _allHistory.Count - 1;
+
+            NotifyUndoRedoState();
+            return true;
+        }
+        private void NotifyUndoRedoState()
+        {
+            OnUndoStateChanged?.Invoke(_currentIndex > -1);
+            OnRedoStateChanged?.Invoke(_allHistory.Count - _currentIndex > 1);
+        }
+    }
+
+    public class ImageData
+    {
+        public byte[] ImageBytes { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+
+    public class TimeMachineHistory 
+    {
+        public TimeMachineHistoryType CommitType;
+        public bool StrokeHasBeenCleared;
+        public StrokeCollection CurrentStroke;
+        public StrokeCollection ReplacedStroke;
+        public byte[] ImageBytes;
+        public double X;
+        public double Y;
+        public double Width;
+        public double Height;
+        public int ImageId;
+        public string ImageSavedName;
+
+        public TimeMachineHistory(StrokeCollection currentStroke, TimeMachineHistoryType commitType, bool strokeHasBeenCleared)
+        { //墨迹
+            CommitType = commitType;
+            CurrentStroke = currentStroke;
+            StrokeHasBeenCleared = strokeHasBeenCleared;
+            ReplacedStroke = null;
+            ImageBytes = null;
+            X = 0;            
+            Y = 0;            
+            Width = 0;        
+            Height = 0;       
+        }
+
+        public TimeMachineHistory(StrokeCollection currentStroke, TimeMachineHistoryType commitType, bool strokeHasBeenCleared, StrokeCollection replacedStroke)
+        { //墨迹替换
+            CommitType = commitType;
+            CurrentStroke = currentStroke;
+            StrokeHasBeenCleared = strokeHasBeenCleared;
+            ReplacedStroke = replacedStroke;
+            ImageBytes = null;
+            X = 0;            
+            Y = 0;            
+            Width = 0;        
+            Height = 0;       
+        }
+
+        public TimeMachineHistory(byte[] imageBytes, double x, double y, double width, double height, TimeMachineHistoryType commitType, int imageId)
+        { // 添加图片
+            CommitType = commitType;
+            CurrentStroke = null;
+            StrokeHasBeenCleared = false;
+            ReplacedStroke = null;
+            ImageBytes = imageBytes;
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            ImageId = imageId;
+        }
+        public TimeMachineHistory(TimeMachineHistoryType commitType, int imageId)
+        { // 删除图片
+            CommitType = commitType;
+            ImageId = imageId;
+        }
+        public TimeMachineHistory(double x, double y, double width, double height, int imageId)
+        { // 变换图片
+            CommitType = TimeMachineHistoryType.ImageChange;
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            ImageId = imageId;
+        }
+    }
+
+    public enum TimeMachineHistoryType
+    {
+        UserInput,
+        ShapeRecognition,
+        Clear,
+        Rotate,
+        ImageInsert,
+        ImageRomove,
+        ImageChange
+    }
+}
